897778f0fad7b44536b36b3da853b3f5f4f7e404
poirier <auriane.poirier@etu.umontpellier.fr>
2023-04-03 20:37:25 +0200

    config routage 1

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
 const app = express();
-const Thing = require('./models/things');
+const stuffRoutes = require('./routes/stuff');
 
 mongoose.connect(process.env.DATABASE_URL,
   { useNewUrlParser: true,
@@ -12,67 +12,19 @@ mongoose.connect(process.env.DATABASE_URL,
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-  
-app.use(express.json());    // pour gérer requète POST, donne le body des requêtes "application/json" sur req
-
-// METHODE CRUD : post/put/delete/get
 
 // header permettant d'accéder à l'API depuis n'importe où
 // ajoute les headers mentionnés aux requêtes envoyées vers l'API
 // envoie requête avec les méthodes GET, POST ...
 //sert à éviter les erreurs de CORS
-app.use((req, res, next) => {
+app.get((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*')
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
 
-//                                  ROUTE POST
-
-app.post('/api/stuff', (req, res, next) => {
-  delete req.body._id;
-  const thing = new Thing({
-    ...req.body
-  });
-  thing.save()
-    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
-    .catch(error => res.status(400).json({ error }));
-});
-
-//                                  ROUTE PUT
-
-
-app.put('/api/stuff/:id', (req, res, next) => {
-  Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
-    .catch(error => res.status(400).json({ error }));
-});
-
-//                                  ROUTE DELETE
-
-app.delete('/api/stuff/:id', (req, res, next) => {
-  Thing.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
-    .catch(error => res.status(400).json({ error }));
-});
-
-//                                  ROUTE GET
-
-// route API
-  app.get('/api/stuff/:id', (req, res, next) => {
-    Thing.findOne({ _id: req.params.id })
-      .then(thing => res.status(200).json(thing))
-      .catch(error => res.status(404).json({ error }));
-  });
-  
-app.use('/api/stuff', (req, res, next) => {
-  Thing.find()
-    .then(things => res.status(200).json(things))
-    .catch(error => res.status(400).json({ error }));
-});
-
-
-
+app.use(express.json());    // pour gérer requète POST, donne le body des requêtes "application/json" sur req
+app.use('/api/stuff', stuffRoutes);
 
 module.exports = app;
