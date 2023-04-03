b1ee6f5a13b134ffcc35dafd2f79a2e127be1c93
poirier <auriane.poirier@etu.umontpellier.fr>
2023-04-03 14:34:56 +0200

    enregistrer/recuperer des données

--- app.js
+++ app.js
@@ -40,6 +40,12 @@ app.post('/api/stuff', (req, res, next) => {
 });
 
   
+app.use('/api/stuff', (req, res, next) => {
+  Thing.find()
+    .then(things => res.status(200).json(things))
+    .catch(error => res.status(400).json({ error }));
+});
+
 //                                  ROUTE GET
 
 
@@ -64,6 +70,13 @@ app.get('/api/stuff', (req, res, next) => {
       },
     ];
     res.status(200).json(stuff);
+  });
+
+
+  app.get('/api/stuff/:id', (req, res, next) => {
+    Thing.findOne({ _id: req.params.id })
+      .then(thing => res.status(200).json(thing))
+      .catch(error => res.status(404).json({ error }));
   });
 
 module.exports = app;
