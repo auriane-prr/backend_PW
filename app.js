7feb063a8af757ecee7658f5d6eef0e419d3c8e7
poirier <auriane.poirier@etu.umontpellier.fr>
2023-04-03 13:17:02 +0200

    premier commit

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ mongoose.connect(process.env.DATABASE_URL,
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-
+  
 app.use(express.json());    // pour gérer requète POST, donne le body des requêtes "application/json" sur req
 
 
